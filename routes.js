2185cd068a5fe2171130f20f3e083dee2fb78ed1
Maggie Oudsema <maggieoudsema@gmail.com>
2021-05-19 21:38:46 -0400

    added delete path

--- routes.js
+++ routes.js
@@ -32,9 +32,9 @@ router.get("/states/:abbrev", async (req, res) => {
 //try catch handles the errors quickly along with not having to wait 20 seconds to time out.
 router.post("/states", async (req, res) => {
   try {
-    const exisiting = await db.one ('SELECT abbrev, name FROM states WHERE abbrev = $(abbrev)', {abbrev: req.body.abbrev});
-    if(exisiting){
-      return res.status(400).send('te state already exists')
+    const exisiting = await db.oneOrNone ('SELECT abbrev, name FROM states WHERE abbrev = $(abbrev)', {abbrev: req.body.abbrev});
+    if (exisiting) {
+      return res.status(400).send("The state already exists");
     }
         await db.none('INSERT INTO states (abbrev, name) VALUES ($(abbrev), $(name))', {
             abbrev: req.body.abbrev,
@@ -65,31 +65,66 @@ router.get("/cities", async (req, res) => {
 //POST - new city
 router.post("/cities", async (req, res) => {
   try {
-    await db.none(
-      "INSERT INTO cities (state_abbrev, name, climate) VALUES ($(state_abbrev), $(name), $(climate))",
-      {
-        state_abbrev: req.body.state_abbrev,
-        name: req.body.name,
-        climate: req.body.climate
+
+      const exisiting = await db.oneOrNone ('SELECT state_abbrev, name, climate FROM cities WHERE name = $(name)', {name: req.body.name});
+
+      if (exisiting){
+        return res.status(400).send('The city already exists.')
       }
-    );
 
-    const city = await db.one(
-      "SELECT state_abbrev, name FROM states WHERE state_abbrev = $(state_abbrev)",
-      { state_abbrev: req.body.abbrev }
-    );
+      const result = await db.none(
+        "INSERT INTO cities (state_abbrev, name, climate) VALUES ($(state_abbrev), $(name), $(climate)) RETURNING id",
+        {
+          state_abbrev: req.body.state_abbrev,
+          name: req.body.name,
+          climate: req.body.climate
+        }
+      );
+        console.log (id);
 
-    res.status(201).json(city);
-  } catch (ex) {
-    console.log(ex);
-    res.status(500).send(ex);
+      const citiy = await db.one(
+        "SELECT state_abbrev, name, climate FROM cities WHERE id = $(id)",
+        { id: result.id }
+      );
+
+      return res.status(201).json(city);
+      
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
   }
 });
 
 router.get("/temperature", async (req, res)=> {
-  res.json(
-    await db.manyOrNone('')
-  )
+    try {
+      const exisiting = await db.oneOrNone(
+        "SELECT city_id, temperature, date FROM temperatures WHERE name = $(name)",
+        { name: req.body.name }
+      );
+
+      if (exisiting) {
+        return res.status(400).send("The city already exists.");
+      }
+
+      const result = await db.none(
+        "INSERT INTO temperatures (city_id, temperature, date) VALUES ($(city_id), $(temperature), $(date)) RETURNING id",
+        {
+          city_id: req.body.city_id,
+          temperature: req.body.temperature,
+          date: req.body.date,
+        }
+      );
+
+      const temperature = await db.one(
+        "SELECT id, city_id, temperature, date FROM temperature WHERE id = $(id)",
+        { id: result.id }
+      );
+
+      return res.status(201).json(temperature);
+    } catch (error) {
+      console.log(error.constraint === 'temperature_city_id_date');
+      res.status(400).send('The temperature already exists for this city and date');
+    }
 })
 
 //get a city by id
@@ -102,5 +137,23 @@ router.get("/cities", async (req, res) => {
   `)
   );
 });
+
+router.delete("/cities/:name", async (req, res) =>{
+  await db.none ('DELETE cities WHERE name = $(name)'), {name: req.params.name};
+
+  return res.status(204).send();
+});
+
+router.delete('/states/:abbrev', async (req, res) => {
+  await db.none('DELETE FROM states WHERE abbrev = $(abbrev)', {abbrev: req.params.abbrev});
+  return res.status(204).send();
+});
+
+router.delete('/temperatures/:id', async (req, res) => {
+  await db.none('DELETE FROM temperatures WHERE id = $(id)', {id: +req.params.id});
+  return res.status(204).send();
+});
+
+
 
 module.exports = router;
